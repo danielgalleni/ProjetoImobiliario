bfb20f1d8b1e956a8f8a1868f1bbb831dffd5315
Daniel <Daniel@10.79.10.134>
2016-10-03 20:46:40 -0300

    Teste de Commit 1 Daniel Galleni 54321

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -5,12 +5,15 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 /**
  *
  * @author Gonzalez
